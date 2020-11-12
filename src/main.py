431078a3971447fea9e810a01cf3b02ff0b9cb59
rish-16 <mail.rishabh.anand@gmail.com>
2020-11-12 10:38:52 +0800

    Add LSTM memory buffer

--- src/main.py
+++ src/main.py
@@ -4,18 +4,26 @@ import numpy as np
 
 from gym_maze.envs import MazeEnv
 from gym_maze.envs.generators import TMazeGenerator
+from gym_maze.envs.Astar_solver import AstarSolver
+
+from mem_buff import MemBuff
 
 maze = TMazeGenerator(3, [5, 3], [3, 3])
 env = MazeEnv(maze, action_type="VonNeumann", render_trace=False)
-
 obs = env.reset()
 
-while True:
-    env.render()
-    action = env.action_space.sample()
+obs_dim = obs.reshape([21, 24, 1]).shape
+n_actions = env.action_space.n
+
+solver = AstarSolver(env, env.goal_states[0])
+
+mem_buff = MemBuff(obs_dim, n_actions)
+
+if not solver.solvable():
+    raise ValueError('The maze is not solvable given the current state and the goal state')
+
+ideal_solution = solver.get_actions()
+
+for action in solver.get_actions():
     obs, reward, done, info = env.step(action)
-    
-    if done:
-        break
-    
-env.close()    
+    env.render()
