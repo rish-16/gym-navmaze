bb9bea525ed7d4c8c0bcdf70a8c67209ab553a92
rish-16 <mail.rishabh.anand@gmail.com>
2021-01-14 14:27:50 +0800

    Add dynamic reward function; Add lookup table for minimum moves to goal; Add logs for 1M timesteps

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import gym
 import gym_maze
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 
 from gym_maze.envs import MazeEnv
 from gym_maze.envs.generators import TMazeGenerator, RandomBlockMazeGenerator
@@ -11,28 +12,29 @@ import ray
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune.registry import register_env
 
-maze = RandomBlockMazeGenerator(maze_size=20, obstacle_ratio=0.2)
-env = MazeEnv(maze, action_type="VonNeumann", render_trace=False)
-walls = env.reset()
+maze = RandomBlockMazeGenerator(maze_size=14, obstacle_ratio=0.2)
+env = MazeEnv(maze, size=14, action_type="VonNeumann", render_trace=False)
 
-obs_dim = walls.shape
-n_actions = env.action_space.n
-print (obs_dim, n_actions)
+num_moves_lookup = [[np.inf for j in range(15)] for i in range(15)]
 
-solver = AstarSolver(env, env.goal_states[0])
-
-if not solver.solvable():
-    raise ValueError('The maze is not solvable given the current state and the goal state')
-
-ideal_solution = solver.get_actions()
-
-# for action in solver.get_actions():
-    # walls, reward, done, info = env.step(action)
+print (env.goal_states[0])
+for s in env.free_spaces:
+    env.init_state = s
+    solver = AstarSolver(env, env.goal_states[0])
+    if solver.solvable():
+        ideal = solver.get_actions()
+        num_moves_lookup[env.init_state[0]][env.init_state[1]] = len(ideal)
     
-ray.init()    
+    env.reset()
+    
+env.lookup_table = num_moves_lookup
+
+print (env.lookup_table)
+    
+ray.init()
 register_env("navmaze", lambda env_config : env)
-    
+
 trainer = PPOTrainer(env="navmaze")
 
-# while True:
-    # print (trainer.train())
+while True:
+    print (trainer.train())
