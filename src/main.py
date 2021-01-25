60391e66d336518872bbe5e7c091bf506e52e15b
rish-16 <mail.rishabh.anand@gmail.com>
2021-01-26 01:18:26 +0800

    Remove RLLib from src; Switch over to vanilla PPO+LSTM implemenation in Torch

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-import gym
-import gym_maze
+import gym, torch, gym_maze
+from torch.distributions import Categorical
 import numpy as np
 import matplotlib.pyplot as plt
 from pprint import pprint
@@ -8,19 +8,28 @@ from gym_maze.envs import MazeEnv
 from gym_maze.envs.generators import TMazeGenerator, RandomBlockMazeGenerator
 from gym_maze.envs.Astar_solver import AstarSolver
 
-import ray
-from ray.rllib.agents.ppo import PPOTrainer
-from ray.tune.registry import register_env
+from methods import PPO_LSTM
+
+def display(mp):
+    for i in range(len(mp)):
+        header = ""
+        for j in range(len(mp[i])):
+            header += "{:^5}".format(mp[i][j])
+        print (header)
 
 maze = RandomBlockMazeGenerator(maze_size=14, obstacle_ratio=0.2)
 env = MazeEnv(maze, size=14, action_type="VonNeumann", render_trace=False)
-
+env.seed(1)
 num_moves_lookup = [[np.inf for j in range(15)] for i in range(15)]
+T_horizon = 20
 
 print (env.goal_states[0])
+
 for s in env.free_spaces:
     env.init_state = s
+    env.reset()
     solver = AstarSolver(env, env.goal_states[0])
+    
     if solver.solvable():
         ideal = solver.get_actions()
         num_moves_lookup[env.init_state[0]][env.init_state[1]] = len(ideal)
@@ -28,13 +37,53 @@ for s in env.free_spaces:
     env.reset()
     
 env.lookup_table = num_moves_lookup
+display(num_moves_lookup)
 
-print (env.lookup_table)
+# ray.init()
+# register_env("navmaze", lambda env_config : env)
+
+# trainer = PPOTrainer(env="navmaze")
+
+# while True:
+#     print (trainer.train())
+
+model = PPO_LSTM()
+score = 0
+
+rewards = []
+success_rate = 0
+
+for ep in range(1000000):
+    h_out = (torch.zeros([1, 1, 32], dtype=torch.float), torch.zeros([1, 1, 32], dtype=torch.float))
+    s = env.reset()
+    done = False
     
-ray.init()
-register_env("navmaze", lambda env_config : env)
-
-trainer = PPOTrainer(env="navmaze")
-
-while True:
-    print (trainer.train())
+    while not done:
+        for t in range(T_horizon):
+            h_in = h_out
+            prob, h_out = model.pi(torch.from_numpy(s).float(), h_in)
+            prob = prob.view(-1)
+            m = Categorical(prob)
+            action = m.sample().item()
+            s_prime, r, done, info = env.step(action)
+            rewards.append(r)
+            
+            model.put_data((s, action, r, s_prime, prob[action].item(), h_in, h_out, done))
+            s = s_prime
+            
+            score += r
+            if done:
+                done = True
+                success_rate += 1
+                break
+                
+        model.train()
+        
+    if (ep % 20 == 0 and ep != 0):
+        print ("# of episode : {} | avg score : {:.1f}".format(ep, score/20))
+        score = 0.0
+        
+env.close()
+print (success_rate)
+plt.plot(list(range(1000000)), rewards)
+plt.show()
--- src/methods.py
+++ src/methods.py
@@ -1,46 +1,99 @@
 import numpy as np
-import tensorflow as tf
-import tensorflow_probability as tfp
-import sonnet as snt
+import torch, time
+from torch import nn
+from torch.nn import functional as F
+from torch.utils import tensorboard
+import torch.optim as optim
 
-class Actor(snt.Module):
-    def __init__(self, dim_in, dim_out, ls_min=-10, ls_max=0):
-        super().__init__()
-        self.ls_max = ls_max
-        self.ls_min = ls_min
-        w_init = 1e-3
-        
-        self.l1 = snt.Linear(256)
-        
-        init = snt.initializers.RandomUniform(-w_init, w_init)
-        self.mu_layer = snt.Linear(dim_out, initializer=init)
-        self.log_std_layer = snt.Linear(dim_out, initializer=init)
-        
-    def __call__(self, state):
-        x = tf.nn.relu(self.l1(state))
-        
-        mu = tf.nn.tanh(self.mu_layer(x))
-        
-        log_std = tf.nn.tanh(self.log_std_layer(x))
-        log_std = 0.5 * self.ls_min + (self.ls_max - self.ls_min) * (log_std + 1)
-                
-        std = tf.exp(log_std)
-        
-        distrib = tfp.distributions.Normal(mu, std)
-        action = dist.sample()
-        
-        return action, distrib    
-                
-class Critic(snt.Module):
+class PPO_LSTM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = snt.Linear(64)
-        w_init = 1e-3
-        init = snt.initializers.RandomUniform(-w_init, w_init)
-        self.out = snt.Linear(1, initializer=init)
+        self.data = []
         
-    def __call__(self, state):
-        x = tf.nn.relu(self.l1(state))
-        val = self.out(x)
+        self.fc1 = nn.Linear(6, 64)
+        self.lstm = nn.LSTM(64, 32)
+        self.fc_pi = nn.Linear(32, 4)
+        self.fc_v = nn.Linear(32, 1)
         
-        return val
+        self.gamma = 0.98
+        self.lmbda = 0.95
+        self.alpha = 0.0005
+        self.eps_clip = 0.1
+        
+        self.optimizer = optim.Adam(self.parameters(), lr=self.alpha)
+        
+    def pi(self, x, hidden):
+        x = F.relu(self.fc1(x))
+        x = x.view(-1, 1, 64)
+        x, lstm_hidden = self.lstm(x, hidden)
+        x = self.fc_pi(x)
+        prob = F.softmax(x, dim=2)
+
+        return prob, lstm_hidden
+        
+    def v(self, x, hidden):
+        x = F.relu(self.fc1(x))
+        x = x.view(-1, 1, 64)
+        x, lstm_hid = self.lstm(x, hidden)
+        v = self.fc_v(x)
+        return v
+        
+    def put_data(self, transition):
+        self.data.append(transition)
+        
+    def make_batch(self):
+        s_lst, a_lst, r_lst, s_prime_lst, prob_a_lst, h_in_lst, h_out_lst, done_lst = [], [], [], [], [], [], [], []
+        for transition in self.data:
+            s, a, r, s_pr, prob_a, h_in, h_out, done = transition
+            
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_pr)
+            prob_a_lst.append([prob_a])
+            h_in_lst.append(h_in)
+            h_out_lst.append(h_out)
+            done_mask = 0 if done else 1
+            done_lst.append([done_mask])
+            
+            s, a, r, s_prime, done_mask, prob_a = torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float),torch.tensor(done_lst, dtype=torch.float), torch.tensor(prob_a_lst)
+            
+            self.data = [] # reset data for new batch
+            
+            return s, a, r, s_prime, done_mask, prob_a, h_in_lst[0], h_out_lst[0]
+            
+    def train(self):
+        s, a, r, s_prime, done_mask, prob_a, (h1_in, h2_in), (h1_out, h2_out) = self.make_batch()
+        first_hidden  = (h1_in.detach(), h2_in.detach())
+        second_hidden = (h1_out.detach(), h2_out.detach())
+
+        for i in range(2):
+            v_prime = self.v(s_prime, second_hidden).squeeze(1)
+            td_target = r + self.gamma * v_prime * done_mask
+            v_s = self.v(s, first_hidden).squeeze(1)
+            delta = td_target - v_s
+            delta = delta.detach().numpy()
+            
+            advantage_lst = []
+            advantage = 0.0
+            
+            for item in delta[::-1]:
+                advantage = self.gamma * self.lmbda * advantage + item[0]
+                advantage_lst.append([advantage])
+            
+            advantage_lst.reverse()
+            advantage = torch.tensor(advantage_lst, dtype=torch.float)
+
+            pi, _ = self.pi(s, first_hidden)
+            pi_a = pi.squeeze(1).gather(1,a)
+            ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))  # a/b == log(exp(a)-exp(b))
+
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1-self.eps_clip, 1+self.eps_clip) * advantage
+            loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(v_s, td_target.detach())
+
+            self.optimizer.zero_grad()
+            loss.mean().backward(retain_graph=True)
+            self.optimizer.step()
+            
+    
